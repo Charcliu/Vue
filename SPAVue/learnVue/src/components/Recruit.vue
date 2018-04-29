1ead417a09e0339bf82a335fe1761e2a2482b28b
Fluently <f_liuchang@163.com>
2018-04-29 15:23:18 +0800

    vue-router

--- SPAVue/learnVue/src/components/Recruit.vue
+++ SPAVue/learnVue/src/components/Recruit.vue
@@ -7,20 +7,32 @@
 
 <script>
 export default {
-  name: 'Recruit',
-  data () {
-    return {
-      msg: 'Recruit Page'
-    }
-  },
-  methods : {
-  	goToUser : function(){
-  		const userId = 123
-		this.$router.push({ name: 'User', params: { userId }}) // -> /user/123
-		//this.$router.push({ path: `/user/${userId}` }) // -> /user/123
-  		//this.$router.push('user/k')*/
-  	}
-  }
+	  name: 'Recruit',
+	  data () {
+	    return {
+	      msg: 'Recruit Page'
+	    }
+	  },
+	  methods : {
+	  	goToUser : function(){
+	  		// 编程式导航
+	  		const userId = 123
+			this.$router.push({ name: 'User', params: { userId }}) // -> /user/123
+			//this.$router.push({ path: `/user/${userId}` }) // -> /user/123
+	  		//this.$router.push('user/k')*/
+	  	}
+	  },
+	  beforeRouteEnter (to, from, next) {
+	    // 在渲染该组件的对应路由被 confirm 前调用
+	    // 不！能！获取组件实例 `this`
+	    // 因为当守卫执行前，组件实例还没被创建
+	    next()
+	  },
+	  beforeRouteLeave (to, from, next) {
+	    // 导航离开该组件的对应路由时调用
+	    // 可以访问组件实例 `this`
+	    next()
+	  }
 }
 </script>
 
--- SPAVue/learnVue/src/router/index.js
+++ SPAVue/learnVue/src/router/index.js
@@ -16,6 +16,7 @@ export default new Router({
       name: 'HelloWorld',
       component: HelloWorld
     },
+    // 嵌套路由
     {
       path: '/about',
       name: 'About',
